cfd693136ca67d301119cf96e496d5f3a9e19df0
Tomoro Saito <s.tomoro0726@gmail.com>
2026-04-19 18:15:00 +0900

    スキーマを確定させた

--- src/v1/index.ts
+++
@@ -1,9 +0,0 @@
-import { loadSpacialMap } from "./utils/loadJson.ts";
-
-const spacialMap = loadSpacialMap("./src/v1/mock.json");
-
-console.log(spacialMap.meta);
-console.log(spacialMap.categories);
-
-console.log(spacialMap.data[spacialMap.categories[0]]);
-console.log(spacialMap.data[spacialMap.categories[0]][0].demensions);
